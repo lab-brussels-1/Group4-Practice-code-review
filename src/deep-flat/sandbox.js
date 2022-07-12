fc76f1b88e049cbb0886cd17db922bf92f0d348d
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2022-07-12 16:05:43 +0200

    update some files

--- src/deep-flat/sandbox.js
+++ src/deep-flat/sandbox.js
@@ -4,22 +4,12 @@ import { deepFlat } from './deep-flat';
 
 console.log(deepFlat(['a', ['b', [['c'], ['d']], 'e']])); // ['a','b','c','d','e']
 
-let arr1 = [1, [2, [3, [4]], 5]];
+const arr1 = [1, [2, [3, [4]], 5]];
 let returnArr = deepFlat(arr1);
 console.log(returnArr); // [1,2,3,4,5]
 
 // empty array and single array into single array
 
-let arr2 = [];
+const arr2 = [];
 let returnArr1 = deepFlat(arr2);
 console.log(returnArr1); // []
-
-// testing errors
-
-let arr3 = 1234;
-let returnArr2 = deepFlat(arr3);
-console.log(returnArr2); // throw new Error
-
-let arr4 = 'hi! how are you';
-let returnArr3 = deepFlat(arr4);
-console.log(returnArr3); // throw new Error
