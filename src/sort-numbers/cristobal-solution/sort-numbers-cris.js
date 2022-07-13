05005e5461d5c8f0d8a2e704bd223451f4fcec66
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-07-13 20:14:21 +0200

    linting errors again

--- src/sort-numbers/cristobal-solution/sort-numbers-cris.js
+++ src/sort-numbers/cristobal-solution/sort-numbers-cris.js
@@ -6,7 +6,7 @@
  * Does not need to support: NaN, Infinity, -Infinity.
  *
  * @param {number[]} [price = []] - The array of numbers to sort.
- * @returns {number[]} A new array with the same numbers, but sorted.
+ * @returns {number[]} A new array with the same numbers but sorted.
  * @example
  *
  * sortNumbers([1.5, 1, -1.5, 0, -1]);
@@ -19,11 +19,11 @@
 
 export const sortingNumbers = (price = []) => {
   // backing up our initial array to avoid any side effects
-  const backingUp = [...price];
+  const backup = [...price];
   // this function sorts the values inside an array in an ascending way
-  backingUp.sort((a, b) => {
+  backup.sort((a, b) => {
     return a - b;
   });
   // the return function will show our new array sorted
-  return backingUp;
+  return backup;
 };
