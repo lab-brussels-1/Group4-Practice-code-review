5b22a8979d4e32145319fd24941fd6c79fa3517f
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-07-13 20:08:33 +0200

    changing plain function to arrow

--- src/sort-numbers/cristobal-solution/sort-numbers-cris.js
+++ src/sort-numbers/cristobal-solution/sort-numbers-cris.js
@@ -21,7 +21,7 @@ export const sortingNumbers = (price = []) => {
   // backing up our initial array to avoid any side effects
   const backingUp = [...price];
   // this function sorts the values inside an array in an ascending way
-  backingUp.sort(function (a, b) {
+  backingUp.sort((a, b) => {
     return a - b;
   });
   // the return function will show our new array sorted
